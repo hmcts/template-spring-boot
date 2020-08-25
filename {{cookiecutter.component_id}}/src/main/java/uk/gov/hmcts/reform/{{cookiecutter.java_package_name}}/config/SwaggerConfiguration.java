8df02e7649423e85c3f308b4b6dfc8cad8150623
Tim Jacomb <tim.jacomb@hmcts.net>
2020-08-25 16:09:47 +0100

    Fix missed template reference

--- {{cookiecutter.component_id}}/src/main/java/uk/gov/hmcts/reform/{{cookiecutter.java_package_name}}/config/SwaggerConfiguration.java
+++ {{cookiecutter.component_id}}/src/main/java/uk/gov/hmcts/reform/{{cookiecutter.java_package_name}}/config/SwaggerConfiguration.java
@@ -7,7 +7,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.demo.Application;
+import uk.gov.hmcts.reform.{{cookiecutter.java_package_name}}.Application;
 
 @Configuration
 @EnableSwagger2
