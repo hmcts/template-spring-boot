09312ad3d9859410b4647dca88b799d306233157
Enda Kelly <enda.kelly@hmcts.net>
2022-04-05 11:47:50 +0100

    update openAPI to use variables

--- skeleton/src/main/java/uk/gov/hmcts/${{ values.product }}/${{ values.component | replace("-", "") }}/config/OpenAPIConfiguration.java
+++ skeleton/src/main/java/uk/gov/hmcts/${{ values.product }}/${{ values.component | replace("-", "") }}/config/OpenAPIConfiguration.java
@@ -14,13 +14,13 @@ public class OpenAPIConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .info(new Info().title("rpe demo")
-                      .description("rpe demo")
+            .info(new Info().title("${{ values.product}}.${{ values.component | replace("-", "") }}")
+                      .description("${{ values.product}}.${{ values.component | replace("-", "") }}")
                       .version("v0.0.1")
                       .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
             .externalDocs(new ExternalDocumentation()
                               .description("README")
-                              .url("https://github.com/hmcts/template-spring-boot"));
+                              .url("https://github.com/hmcts/${{ values.product}}.${{ values.component | replace("-", "") }}"));
     }
 
 }
