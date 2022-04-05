24942f4cf81ff977db054bb96cb19e24623989d2
Enda Kelly <enda.kelly@hmcts.net>
2022-04-05 11:12:29 +0100

    update dependencies

---
+++ skeleton/src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+/**
+ * Default endpoints per application.
+ */
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> welcome() {
+        return ok("Welcome to application");
+    }
+}
